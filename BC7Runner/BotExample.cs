d79ae504b54afab8cff91fc91d606f3494e63688
bytinggames <julian@bytinggames.com>
2024-07-21 21:04:57 +0200

    +

--- BC7Runner/BotExample.cs
+++ BC7Runner/BotExample.cs
@@ -10,45 +10,49 @@
 
         public override Disc Step1_FirstDisc()
         {
-            // TODO: add some logic here
+            // TODO
 
-            // get data by typing "game."
-            // example for getting all opponents: game.GetAliveOpponentBots(ID);
-
-            return Disc.Skull;
-            //return Disc.Flower;
+            // example: always return a skull if there are less than 3 bots alive:
+            if (game.GetAliveOpponentBots(ID).Count < 3)
+            {
+                return Disc.Skull;
+            }
+            else
+            {
+                return Disc.Flower;
+            }
         }
 
         public override DiscOrBet Step2A_DiscOrBet()
         {
-            // TODO: add some nonsense here
+            // TODO
 
             return DiscOrBet.Bet(3);
-            //return DiscOrBet.Flower();
-            //return DiscOrBet.Skull();
+            //              .Flower();
+            //              .Skull();
         }
 
         public override IncreaseOrPass Step2B_IncreaseOrPass(int heighestBet)
         {
-            // TODO: add some sense here
+            // TODO
 
             return IncreaseOrPass.Pass();
-            //return IncreaseOrPass.Bet(3);
+            //                   .Bet(3);
         }
 
         public override int Step3_ChoosePlayerToFlip1Disc(int[] playerIDsToChooseFrom)
         {
-            // TODO: add some thing here
+            // TODO
 
             return playerIDsToChooseFrom[0];
         }
 
         public override Disc OnFail_ChooseOwnDiscToDestroy()
         {
-            // TODO: not that important... I think? Not sure, you decide!
+            // not that important... I think? Not sure, you decide!
 
             return Disc.Flower;
-            //return Disc.Skull;
+            //         .Skull;
         }
     }
 }
